4f94382c6a94b0b742798d47acb945fbae7d460d
Joshua <j-andriason@live.fr>
2021-03-01 16:45:37 +0100

    signin

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Signin from './screens/Signin';
 
-export default function App() {
+export default function App(props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Signin/>
     </View>
   );
 }
