4b05f1ebd86d71b912075bc2d8a40dabbe1f46b9
hajarCapsule <h.mounjib@gmail.com>
2021-03-02 09:53:03 +0100

    button bienvenue

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
 import Signin from './screens/Signin';
 import HeaderScreen from './screens/HeaderScreen'
+import AccueilScreen from './screens/AccueilScreen'
+
+const Stack = createStackNavigator();
 
 export default function App(props) {
   return (
 <>
 <HeaderScreen/>
-    <View style={styles.container}>
-   <Signin/>
-    </View>
+   <NavigationContainer >
+     <Stack.Navigator screenOptions={{headerShown: false}}>
+       <Stack.Screen name="Home" component={AccueilScreen} />
+       <Stack.Screen name="Signin" component={Signin} />
+     </Stack.Navigator>
+   </NavigationContainer>
+
+
+
     </>
   );
 }
---
+++ screens/AccueilScreen.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {View,StyleSheet,Button,Image} from 'react-native';
+import { Text } from 'react-native-elements';
+
+
+function AcceuilScreen(props) {
+
+    
+    return (
+   <View style={styles.container}>
+     <View style= {{width : '100%'}}>
+         <Image source={{url: 'https://res.cloudinary.com/dgv5agwfj/image/upload/v1614590356/Hotel%20des%20Deux-%C3%8Eles%20%28Room%20Directory%29/3W8A7073_hotel_des_deux_iles_bd_gqbwwd.jpg'}}
+         style ={{ width : '100%', height: 200, padding: 0, objectFit: 'fill' }}
+         />
+    </View>
+  <Text style={styles.textCenter}>Vous êtes bien arrivé à Paris :
+là où l’esprit reste vif, prêt à se laisser 
+   surprendre et surtout à se laisser voguer. </Text>
+   
+   <Text style={styles.textBold}>Bienvenue dans notre hôtel… </Text>
+   
+   <Text style={styles.textBold}>Commencez dès maintenant en vous connectant 
+   à notre appli qui vous servira tout au long de votre séjour !</Text>
+
+   <Button
+            color='#AADEC0'
+            title="C'est parti"
+            type="solid"
+            onPress={() => {props.navigation.navigate('Signin')}}
+        />
+
+
+   </View>
+  );
+}
+
+
+  
+export default AcceuilScreen
+
+const styles =StyleSheet.create({
+  textBold:{
+    marginTop: 20,fontWeight:'bold',textAlign:'center',marginBottom:20
+  },textCenter:{
+    textAlign:'center'
+  }, container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
