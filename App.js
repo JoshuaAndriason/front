0d029264be7675917de693b919545a3c90b7b4e3
Gwendoline Pedurand <gwendoline.pedurand@gmail.com>
2021-03-02 09:30:18 +0100

    ras morningnews

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Signin from './screens/Signin';
-import HeaderScreen from './screens/HeaderScreen'
+import HeaderScreen from './screens/HeaderScreen';
+
 
 export default function App(props) {
   return (
