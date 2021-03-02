12b72026bc5f03b78194ca73a0ff222f80ff23e9
tracy <tracyzumbu@lilo.org>
2021-03-02 09:27:16 +0100

    added pageTemplate

---
+++ components/pageTemplate.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
+import {Button, ListItem, Input} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
