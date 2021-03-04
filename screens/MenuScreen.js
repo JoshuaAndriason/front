fd199b397647541e7f042c52f8ce9ca1e5931e20
hajarCapsule <h.mounjib@gmail.com>
2021-03-04 09:59:11 +0100

    Menu joshua formik

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import { Button, TextInput, View, Text} from 'react-native';
-
-
+/* import { Formik} from 'formik';
+import { RadioButton } from 'react-native-paper';
+ */
 
 
 export default function MenuScreen(props){
