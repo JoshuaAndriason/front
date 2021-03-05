3c5474b1b207a87985c797afd119d8286dbe4918
hajarCapsule <h.mounjib@gmail.com>
2021-03-05 15:00:21 +0100

    josh badge

--- screens/InscriptionScreen.js
+++ screens/InscriptionScreen.js
@@ -38,12 +38,12 @@ export function InscriptionScreen(props) {
     var handleSubmitSignup = async () => {
     
     
-      const data = await fetch(`http://${IPadress}/sign-up`, {
+      const data = await fetch(`http://${IPadress}:3000/sign-up`, {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: `lastnameFromFront=${lastNameSignUp}&emailFromFront=${emailSignUp}&roomNumberFromFront=${roomNumberSignUp}`
       })
-
+console.log(IPadress)
       const body = await data.json()
      console.log(body)
      if(body.result == true){
@@ -58,7 +58,7 @@ export function InscriptionScreen(props) {
 
     var handleSubmitSignin = async () => {
  
-      const data = await fetch('http://172.17.1.187:3000/sign-in', {
+      const data = await fetch(`http://${IPadress}:3000/sign-in`, {
         method: 'POST',
         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
         body: `emailFromFront=${signInEmail}&lastnameFromFront=${signInName}&roomNumberFromFront=${signInRoom}`
