7799936dba8e7f2a86143087b2050e9a719481da
Gwendoline Pedurand <gwendoline.pedurand@gmail.com>
2021-03-08 15:39:15 +0100

    EventScreenDetails finish

--- screens/BienvenueScreen.js
+++ screens/BienvenueScreen.js
@@ -35,7 +35,7 @@ console.log('im token Bienvenue',isExist)
 
   <TouchableOpacity
     style={styles.button}
-    onPress={() => {props.navigation.navigate('Inscription')}}>
+    onPress={() => {props.navigation.navigate('Home')}}>
     <Text>Bienvenue</Text>
   </TouchableOpacity>
 
