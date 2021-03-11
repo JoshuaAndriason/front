4fb97ad8682abe55b3b1b878c17bd0e4d13bba96
Frederic PRINCE <77095665+frederic-prince@users.noreply.github.com>
2021-03-11 11:35:23 +0100

    Commentaires OK et resize Chat

--- screens/MorningNewsScreen.js
+++ screens/MorningNewsScreen.js
@@ -1,3 +1,4 @@
+{/* Import components */}
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, View, Text} from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements'
@@ -8,6 +9,7 @@ function MorningNewsScreen(props) {
 
   const [sourceList, setSourceList] = useState([])
 
+// Application of a useEffect to newsapi.org to collect articles
   useEffect(() => {
     const APIResultsLoading = async() => {
       const data = await fetch('https://newsapi.org/v2/sources?language=fr&country=fr&apiKey=b32c8b844d1243b1a7998d8228910f50')
@@ -19,10 +21,12 @@ function MorningNewsScreen(props) {
   }, [])
   console.log(sourceList)
 
+
   return (
     <View >
       <HomeImage uri="https://res.cloudinary.com/dgv5agwfj/image/upload/v1614590349/Hotel%20des%20Deux-%C3%8Eles%20%28Room%20Directory%29/3W8A6914_hotel_des_deux_iles_bd_e2lur1.jpg"/>
 {
+// Setup for Sources of aricles Card (title + description) via index
     sourceList.map((l, i) => (
       <ListItem key={i} bottomDivider>
 
@@ -39,7 +43,6 @@ function MorningNewsScreen(props) {
 </View>
   );
 }
-
 function mapDispatchToProps(dispatch) {
   return {
     onSubmitSource: function(idArticle) { 
@@ -51,7 +54,7 @@ export default connect(
   null,mapDispatchToProps)(MorningNewsScreen)
 
 
-
+// Style elements
 const styles = StyleSheet.create({
   container: {
     flex: 1,
